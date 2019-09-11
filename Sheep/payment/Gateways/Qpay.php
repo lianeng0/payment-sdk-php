fd0cb506632a93ddf937d55a2d89f7a9600e50bb
lovan <lovan_zhang@163.com>
2019-09-11 16:14:58 +0800

    修复未使用证书时的报错

--- Sheep/payment/Gateways/Qpay.php
+++ Sheep/payment/Gateways/Qpay.php
@@ -153,7 +153,7 @@ abstract class Qpay extends GatewayInterface
     {
         $result = $this->fromXml(
             $this->post($this->baseUrl . $url, $this->toXml($this->payload),
-                $cert ? ['ssl_cer' => $this->config->get('ssl_cer', ''), 'ssl_key' => $this->config->get('ssl_key', '')]: '')
+                $cert ? ['ssl_cer' => $this->config->get('ssl_cer', ''), 'ssl_key' => $this->config->get('ssl_key', '')]: null)
         );
         halt($result);
         if (!isset($result['return_code']) || $result['return_code'] !== 'SUCCESS' || $result['result_code'] !== 'SUCCESS') {
